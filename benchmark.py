43f87bc3a4fe0c389a3161d6269ac900727ecd6c
Laweens <laweensfrancois@gmail.com>
2023-09-19 09:50:53 -0400

    see what i remember

---
+++ benchmark.py
@@ -0,0 +1,38 @@
+#Exercise 1
+
+print("Laweens")
+
+#exercise 2
+
+Favoriteshow = "Regular Show"
+print(Favoriteshow)
+
+#exercise 3 
+
+height = 6
+age = 16 
+
+print (height * age)
+
+#exercise 4 
+print ("Im going to break")
+
+#exercise 5
+list = ("pizza,burgers,popeyes,chick-fil-a ")
+print(list)
+
+#exercise 6 
+if age > 21 :
+ print ("You can rent a car")
+else: print ("Your beat")
+
+#exercise 7
+name = ("")
+input (name)
+print (name)
+
+#exercise #8 
+for item in list : print (list)
+
+#exercise 9 
+while height > 7 : print ("7 footer and above")
